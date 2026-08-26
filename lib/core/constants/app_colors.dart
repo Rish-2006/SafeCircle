24e4614bc418653e6970821dbd4e69756d7451f5
Rish-2006 <rishitedu5@gmail.com>
2026-08-26 10:29:12 +0530

    style(design-system): add AppColors design tokens class

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color background = Color(0xFF120A1F);
+  static const Color surface = Color(0xFF1E1332);
+  static const Color surfaceAlt = Color(0xFF241A3D);
+  static const Color border = Color(0xFF35244F);
+
+  static const Color primary = Color(0xFF9B7EDE);
+  static const Color primaryDark = Color(0xFF6B4FA0);
+  static const Color accent = Color(0xFFC4A6FF);
+
+  static const Color textPrimary = Color(0xFFF3EFFF);
+  static const Color textSecondary = Color(0xFFB3A3D9);
+  static const Color textMuted = Color(0xFF7C6BA0);
+
+  static const Color success = Color(0xFF4ADE80);
+  static const Color warning = Color(0xFFFBBF24);
+  static const Color danger = Color(0xFFF87171);
+  static const Color infoBlue = Color(0xFF7DD3FC);
+  static const Color coral = Color(0xFFFF8FA3);
+}
