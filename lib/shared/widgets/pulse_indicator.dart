f8f933f7a05e78d883121fb3fea11b5a94c1b002
Rish-2006 <rishitedu5@gmail.com>
2026-08-26 10:29:18 +0530

    feat(design-system): add PulseIndicator micro-animation component

---
+++ lib/shared/widgets/pulse_indicator.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import '../../core/constants/app_colors.dart';
+
+class PulseIndicator extends StatefulWidget {
+  final Color color;
+  final double size;
+
+  const PulseIndicator({
+    super.key,
+    this.color = AppColors.success,
+    this.size = 12.0,
+  });
+
+  @override
+  State<PulseIndicator> createState() => _PulseIndicatorState();
+}
+
+class _PulseIndicatorState extends State<PulseIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: 0.6, end: 1.2).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: _animation.value,
+          child: Container(
+            width: widget.size,
+            height: widget.size,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: widget.color,
+              boxShadow: [
+                BoxShadow(
+                  color: widget.color.withOpacity(0.6),
+                  blurRadius: 8,
+                  spreadRadius: 2,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
